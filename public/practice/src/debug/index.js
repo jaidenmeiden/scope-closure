7851d2536993fd1ad6fbba4381b6feb10fdff392
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-23 23:25:33 +0100

    feat: Debugger

---
+++ public/practice/src/debug/index.js
@@ -0,0 +1,15 @@
+var a = 'Hello';
+
+function hello() {
+    let b = 'Hello world!';
+    const  c = 'Hello new world!!'
+    if (true) {
+        let d = 'Hello new world!!!';
+        debugger
+    }
+}
+
+hello();
+
+console.log("Copy the code above!");
+console.log("Put into Chrome browser =>", "about:blank");
