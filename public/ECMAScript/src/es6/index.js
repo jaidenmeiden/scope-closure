81fc864abd38b979248939359adc1570b3cc6797
Jaiden Riaño <jriano@aiudo.es>
2022-01-13 12:55:35 +0100

    feat: Let, const, multiline and propagation

--- public/ECMAScript/src/es6/index.js
+++ public/ECMAScript/src/es6/index.js
@@ -1,3 +1,5 @@
+console.log('##########################################################################################################');
+console.log('################################################ Functions ###############################################');
 // Function before ES6
 function newFunction(name, age, country) {
     var name = name || 'Mr. Jaiden';
@@ -23,6 +25,8 @@ console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2);
 
+console.log('##########################################################################################################');
+console.log('################################################ Multiline ###############################################');
 // Multiline before ES6
 let lorem = "Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías  \n"
 + "o de borradores de diseño para probar el diseño visual antes de insertar el texto final."
@@ -31,10 +35,44 @@ console.log(lorem);
 let lorem2 = `Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías 
 o de borradores de diseño para probar el diseño visual antes de insertar el texto final.`
 console.log(lorem2);
-
+console.log('##########################################################################################################');
+console.log('############################################## Destructuring #############################################');
 // The destructuring syntax is a JavaScript expression that allows you to unpack the values of arrays or properties of objects into different variables.
 let person = {
     'name': 'Jaiden',
     'age': 37,
     'country': 'COL',
 }
+
+console.log(person.name, person.age);
+
+let { name, age, country} = person;
+
+console.log(name, age, country);
+console.log('##########################################################################################################');
+console.log('############################################### propagation ##############################################');
+
+let team1 = ['Miriam', 'Nicole', 'Celia'];
+let team2 = ['Diana', 'Adriana', 'Leidy', 'Natalia'];
+
+let education = ['Jaiden', ...team1, ...team2]
+
+console.log(education)
+console.log('##########################################################################################################');
+console.log('############################################## Let and const #############################################');
+
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = "Global Let";
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+//console.log(globalLet);//Error
+
+const a = 'b'
+//a = 'c'//Error
+console.log(a)
