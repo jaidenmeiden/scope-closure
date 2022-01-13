cabcd96875934e97ed464403bf5ddc7a31fbed5f
Jaiden Riaño <jriano@aiudo.es>
2022-01-13 16:55:32 +0100

    feat: Add Generators

--- public/ECMAScript/src/es6/classes.js
+++ public/ECMAScript/src/es6/classes.js
@@ -32,3 +32,37 @@ graph.options = {
 graph.draw();
 console.log(cube(3)); // 27
 console.log(foo);    // 4.555806215962888
+
+console.log('##########################################################################################################');
+console.log('############################################## Generators ################################################');
+
+function* talking(value) {
+    if(value) {
+        yield 'Good way'
+    }
+    if(value) {
+        yield 'You are the best!'
+    }
+}
+
+const generatorT = talking(true);
+console.log(generatorT.next().value)
+console.log(generatorT.next().value)
+console.log(generatorT.next().value)
+
+
+console.log('############################################## Fibonacci #################################################');
+
+function* Fibonacci() {
+    let [a, b] = [0, 1]
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b]
+    }
+}
+
+const Initial = Fibonacci();
+
+for (let i = 0; i < 50; i++) {
+    console.log(Initial.next().value);
+}
