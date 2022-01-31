d97df56bfecb646ce6bfbcee0ba621328f89a38d
Jaiden Riaño <jriano@aiudo.es>
2022-01-31 17:32:12 +0100

    feat: Add EcMAScript 7 examples

---
+++ public/ECMAScript/src/es7/index.js
@@ -0,0 +1,23 @@
+console.log('##########################################################################################################');
+console.log('############################################# Array Functions ############################################');
+let numbers = [1,2,3,7,8];
+
+if (numbers.includes(7)) {
+    console.log("File is into array!");
+} else {
+    console.log("File isn't into array!");
+}
+
+if (numbers.includes(9)) {
+    console.log("File is into array!");
+} else {
+    console.log("File isn't into array!");
+}
+
+console.log('##########################################################################################################');
+console.log('############################################## Pow inclusion #############################################');
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
