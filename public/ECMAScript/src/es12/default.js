9783cf48ba405c6695720cf7baf38da0b3104228
Jaiden Riaño <jriano@aiudo.es>
2022-02-02 16:24:29 +0100

    feat: Add OAssignment operators examples from EcMAScript 12

--- public/ECMAScript/src/es12/default.js
+++ public/ECMAScript/src/es12/default.js
@@ -81,6 +81,143 @@ console.log(subMessage1.getPrivateStaticMethod());
 console.log(subMessage1.getPrivateStaticMethod());
 
 console.log('##########################################################################################################');
+console.log('############################################## Operators #################################################');
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
+
+console.log('Arithmetic operators in JavaScript');
+
+let x = 5;
+let y = 3;
+
+// addition
+console.log('x + y = ', x + y);  // 8
+
+// subtraction
+console.log('x - y = ', x - y);  // 2
+
+// multiplication
+console.log('x * y = ', x * y);  // 15
+
+// division
+console.log('x / y = ', x / y);  // 1.6666666666666667
+
+// remainder
+console.log('x % y = ', x % y);   // 2
+
+// increment
+console.log('++x = ', ++x); // x is now 6
+console.log('x++ = ', x++); // prints 6 and then increased to 7
+console.log('x = ', x);     // 7
+
+// decrement
+console.log('--x = ', --x); // x is now 6
+console.log('x-- = ', x--); // prints 6 and then decreased to 5
+console.log('x = ', x);     // 5
+
+//exponentiation
+console.log('x ** y =', x ** y);
+
+console.log('Comparison operators in JavaScript');
+
+// equal operator
+console.log('2 == 2', 2 == 2); // true
+console.log('2 == "2"', 2 == "2"); // true
+
+// not equal operator
+console.log('3 != 2', 3 != 2); // true
+console.log('"hello" != "Hello"', "hello" != "Hello"); // true
+
+// strict equal operator
+console.log('2 === 2', 2 === 2); // true
+console.log('2 === "2"', 2 === "2"); // false
+
+// strict not equal operator
+console.log('2 !== "2"', 2 !== "2"); // true
+console.log('2 !== 2', 2 !== 2); // false
+
+console.log('Logical Operators in JavaScript');
+
+// logical AND
+console.log('true && true', true && true); // true
+console.log('true && false', true && false); // false
+
+// logical OR
+console.log('true || false', true || false); // true
+
+// logical NOT
+console.log('!true', !true); // false
+
+let a = 12;
+let  b = 25;
+console.log('Bitwise AND Operator');
+
+let result = a & b;
+console.log('a & b', result);
+
+console.log('Bitwise OR Operator');
+
+result = a | b;
+console.log('a | b', result); // 29
+
+console.log('Bitwise XOR Operator');
+
+result = a ^ b;
+console.log('a ^ b', result); // 21
+
+console.log('Bitwise NOT Operator');
+b = 12;
+result = ~b;
+
+console.log('~b', result); // -13
+
+console.log('Left shift');
+a = 8;
+b = 1;
+
+result = a << b;
+
+// 1 ( 00000000000000000000000000010000 )
+console.log('a << b', result);
+
+console.log('Sign-propagating right shift');
+
+// 11111111111111111111111111111101
+a = 8;
+b = 1;
+let c = -3;
+
+result = a >> b;
+let result1 = c >> b;
+
+// 4 (00000000000000000000000000000100)
+console.log('a >> b', result);
+
+// -1 (11111111111111111111111111111111)
+console.log('c >> b', result1);
+
+console.log('Zero-fill right shift');
+a = 8;
+b = 1;
+// 11111111111111111111111111111101
+c = -3;
+
+result = a >>> b;
+result1 = c >>> b;
+
+// 4 (00000000000000000000000000000100)
+console.log('a >>> b', result);
+
+// 1073741823 (00111111111111111111111111111111)
+console.log('c >>> b', result1);
+console.log('Assignment operators');
+let isTrue = true;
+let isFalse = false;
+console.log('isTrue &&= isFalse', isTrue &&= isFalse);
+console.log('isTrue ||= isFalse', isTrue ||= isFalse);
+isTrue = undefined;
+console.log('isTrue ??= isFalse', isTrue ??= isFalse);
+
+console.log('##########################################################################################################');
 console.log('############################################# Promise Any ################################################');
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/any
 
